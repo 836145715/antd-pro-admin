b9102cfda38bef0c82ed6235e5e45f36bbbbeb1f
zmx <836145715@qq.com>
2025-12-11 16:15:21 +0800

    升级路由

--- src/layout/BasicLayout.tsx
+++ src/layout/BasicLayout.tsx
@@ -1,14 +1,11 @@
 import {
-  ChromeFilled,
-  CrownFilled,
   GithubFilled,
   InfoCircleFilled,
   QuestionCircleFilled,
-  SmileFilled,
-  TabletFilled,
 } from "@ant-design/icons";
 import { PageContainer, ProCard, ProLayout } from "@ant-design/pro-components";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { menuRoutes } from "../routes/index";
 
 const BasicLayout = () => {
   const navigate = useNavigate();
@@ -23,93 +20,7 @@ const BasicLayout = () => {
     >
       <ProLayout
         siderWidth={216}
-        route={{
-          path: "/",
-          routes: [
-            {
-              path: "/welcome",
-              name: "欢迎",
-              icon: <SmileFilled />,
-            },
-            {
-              path: "/dashboard",
-              name: "Dashboard",
-              icon: <SmileFilled />,
-            },
-            {
-              path: "/admin",
-              name: "管理页",
-              icon: <CrownFilled />,
-              access: "canAdmin",
-              routes: [
-                {
-                  path: "/admin/sub-page1",
-                  name: "一级页面",
-                  icon: "https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg",
-                },
-                {
-                  path: "/admin/sub-page2",
-                  name: "二级页面",
-                  icon: <CrownFilled />,
-                },
-                {
-                  path: "/admin/sub-page3",
-                  name: "三级页面",
-                  icon: <CrownFilled />,
-                },
-              ],
-            },
-            {
-              name: "列表页",
-              icon: <TabletFilled />,
-              path: "/list",
-              routes: [
-                {
-                  path: "/list/sub-page",
-                  name: "列表页面",
-                  icon: <CrownFilled />,
-                  routes: [
-                    {
-                      path: "/list/sub-page/sub-sub-page1",
-                      name: "一一级列表页面",
-                      icon: <CrownFilled />,
-                    },
-                    {
-                      path: "/list/sub-page/sub-sub-page2",
-                      name: "一二级列表页面",
-                      icon: <CrownFilled />,
-                    },
-                    {
-                      path: "/list/sub-page/sub-sub-page3",
-                      name: "一三级列表页面",
-                      icon: <CrownFilled />,
-                    },
-                  ],
-                },
-                {
-                  path: "/list/sub-page2",
-                  name: "二级列表页面",
-                  icon: <CrownFilled />,
-                },
-                {
-                  path: "/list/sub-page3",
-                  name: "三级列表页面",
-                  icon: <CrownFilled />,
-                },
-              ],
-            },
-            {
-              path: "/form",
-              name: "表单页",
-              icon: <CrownFilled />,
-            },
-            {
-              path: "https://ant.design",
-              name: "Ant Design 官网外链",
-              icon: <ChromeFilled />,
-            },
-          ],
-        }}
+        route={menuRoutes}
         location={{
           pathname: location.pathname,
         }}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,11 @@
+import {
+  ChromeFilled,
+  CrownFilled,
+  SmileFilled,
+  TabletFilled,
+} from "@ant-design/icons";
+import type { MenuDataItem } from "@ant-design/pro-components";
+import type { RouteObject } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 import BasicLayout from "../layout/BasicLayout";
 import Welcome from "../pages/Welcome";
@@ -9,82 +17,115 @@ import Dashboard from "../pages/Dashboard";
 import NotFound from "../pages/404";
 import Form from "../pages/Form";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <BasicLayout />,
-    children: [
-      {
-        index: true,
-        element: <Welcome />,
-      },
-      {
-        path: "welcome",
-        element: <Welcome />,
-      },
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "admin",
-        element: <Admin />,
-        children: [
-          {
-            path: "sub-page1",
-            element: <AdminSubPage1 />,
-          },
-          {
-            path: "sub-page2",
-            element: <AdminSubPage1 />,
-          },
-          {
-            path: "sub-page3",
-            element: <AdminSubPage1 />,
-          },
-        ],
-      },
-      {
-        path: "list",
-        element: <ListTableList />,
-        children: [
-          {
-            path: "sub-page",
-            children: [
-              {
-                path: "sub-sub-page1",
-                element: <ListSubSubPage1 />,
-              },
-              {
-                path: "sub-sub-page2",
-                element: <ListSubSubPage1 />,
-              },
-              {
-                path: "sub-sub-page3",
-                element: <ListSubSubPage1 />,
-              },
-            ],
-          },
-          {
-            path: "sub-page2",
-            element: <ListSubSubPage1 />,
-          },
-          {
-            path: "sub-page3",
-            element: <ListSubSubPage1 />,
-          },
-        ],
-      },
-      {
-        path: "form",
-        element: <Form />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+export type MenuConfig = MenuDataItem & RouteObject;
 
-export default router;
+const menuRoutes: MenuConfig = {
+  path: "/",
+  element: <BasicLayout />,
+  children: [
+    {
+      index: true,
+      element: <Welcome />,
+    },
+    {
+      path: "/welcome",
+      name: "欢迎",
+      icon: <SmileFilled />,
+      element: <Welcome />,
+    },
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      icon: <SmileFilled />,
+      element: <Dashboard />,
+    },
+    {
+      path: "/admin",
+      name: "管理页",
+      icon: <CrownFilled />,
+      access: "canAdmin",
+      children: [
+        {
+          index: true,
+          element: <Admin />,
+        },
+        {
+          path: "/admin/sub-page1",
+          name: "一级页面",
+          icon: "https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg",
+          element: <AdminSubPage1 />,
+        },
+        {
+          path: "/admin/sub-page2",
+          name: "二级页面",
+          icon: <CrownFilled />,
+          element: <AdminSubPage1 />,
+        },
+        {
+          path: "/admin/sub-page3",
+          name: "三级页面",
+          icon: <CrownFilled />,
+          element: <AdminSubPage1 />,
+        },
+      ],
+    },
+    {
+      name: "列表页",
+      icon: <TabletFilled />,
+      path: "/list",
+      children: [
+        {
+          path: "/list/sub-page",
+          name: "列表页面",
+          icon: <CrownFilled />,
+          children: [
+            {
+              path: "/list/sub-page/sub-sub-page1",
+              name: "一一级列表页面",
+              icon: <CrownFilled />,
+              element: <ListTableList />,
+            },
+            {
+              path: "/list/sub-page/sub-sub-page2",
+              name: "一二级列表页面",
+              icon: <CrownFilled />,
+              element: <ListSubSubPage1 />,
+            },
+            {
+              path: "/list/sub-page/sub-sub-page3",
+              name: "一三级列表页面",
+              icon: <CrownFilled />,
+              element: <ListSubSubPage1 />,
+            },
+          ],
+        },
+        {
+          path: "/list/sub-page2",
+          name: "二级列表页面",
+          icon: <CrownFilled />,
+          element: <ListSubSubPage1 />,
+        },
+        {
+          path: "/list/sub-page3",
+          name: "三级列表页面",
+          icon: <CrownFilled />,
+          element: <ListSubSubPage1 />,
+        },
+      ],
+    },
+    {
+      path: "/form",
+      name: "表单页",
+      icon: <CrownFilled />,
+      element: <Form />,
+    },
+    {
+      path: "https://ant.design",
+      name: "Ant Design 官网外链",
+      icon: <ChromeFilled />,
+      element: <NotFound />,
+    },
+  ],
+};
+const router = createBrowserRouter([menuRoutes]);
+export { router, menuRoutes };
